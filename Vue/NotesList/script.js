96e2675b0a166bfd40496d6b5a3ced74fd3e4125
ilyazen <79088992+ilyazen@users.noreply.github.com>
2021-03-03 17:52:43 +0800

    Add files via upload

--- Vue/NotesList/script.js
+++ Vue/NotesList/script.js
@@ -5,17 +5,33 @@ const app = new Vue({
      placeholderText:"Введите заметку",
      notes:[],
      inputValue:'',
+     noteDone:0,
    
    },
    methods:{
       addNote () {
+         
+      if(!this.notes.includes(this.inputValue)) {
          this.notes.push(this.inputValue);
+      }
+
       },
+      
       removeNote(idx) {
          this.notes.splice(idx,1)
       },
+      checkStatus(event){
+         let currentCheckboxStatus = event.target.checked;
+         if(currentCheckboxStatus) {
+            this.noteDone ++;
+         } else {
+            this.noteDone --;
+         }
+      }
+
       
 },
+
 
 
  })
